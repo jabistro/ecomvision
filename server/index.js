f8b91fc8cf7db6948d200f4994a2a3defadc6947
John Allan Hinds <jabistro@gmail.com>
2023-03-10 17:29:53 -0500

    first commit

---
+++ server/index.js
@@ -0,0 +1,14 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import helmet from "helmet";
+import morgan from "morgan";
+
+/* CONFIGURATION */
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
