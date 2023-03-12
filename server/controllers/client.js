ab0383ec52074b71d2c64039769befa13bd4cef1
John Allan Hinds <jabistro@gmail.com>
2023-03-12 15:39:43 -0400

    geography added to backend

--- server/controllers/client.js
+++ server/controllers/client.js
@@ -2,7 +2,7 @@ import Product from "../models/Product.js";
 import ProductStat from "../models/ProductStat.js";
 import User from "../models/User.js";
 import Transaction from "../models/Transaction.js";
-// import getCountryIso3 from "country-iso-2-to-3";
+import getCountryIso3 from "country-iso-2-to-3";
 
 export const getProducts = async (req, res) => {
   try {
@@ -74,27 +74,27 @@ export const getTransactions = async (req, res) => {
   }
 };
 
-// export const getGeography = async (req, res) => {
-//   try {
-//     const users = await User.find();
+export const getGeography = async (req, res) => {
+  try {
+    const users = await User.find();
 
-//     const mappedLocations = users.reduce((acc, { country }) => {
-//       const countryISO3 = getCountryIso3(country);
-//       if (!acc[countryISO3]) {
-//         acc[countryISO3] = 0;
-//       }
-//       acc[countryISO3]++;
-//       return acc;
-//     }, {});
+    const mappedLocations = users.reduce((acc, { country }) => {
+      const countryISO3 = getCountryIso3(country);
+      if (!acc[countryISO3]) {
+        acc[countryISO3] = 0;
+      }
+      acc[countryISO3]++;
+      return acc;
+    }, {});
 
-//     const formattedLocations = Object.entries(mappedLocations).map(
-//       ([country, count]) => {
-//         return { id: country, value: count };
-//       }
-//     );
+    const formattedLocations = Object.entries(mappedLocations).map(
+      ([country, count]) => {
+        return { id: country, value: count };
+      }
+    );
 
-//     res.status(200).json(formattedLocations);
-//   } catch (error) {
-//     res.status(404).json({ message: error.message });
-//   }
-// };
+    res.status(200).json(formattedLocations);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
