14da921458752cd2fbe12142879547e937f56a5b
John Allan Hinds <jabistro@gmail.com>
2023-03-11 19:04:30 -0500

    api.js added

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -31,7 +31,7 @@ import {
 import { useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import FlexBetween from "./FlexBetween";
-import profileImage from "assets/jah-profile.png";
+import profileImage from "assets/profile.jpeg";
 
 const navItems = [
   {
