22f8eb09a75f8f2e0a13243a551ab43d6a6d8723
John Allan Hinds <jabistro@gmail.com>
2023-03-12 16:38:00 -0400

    daily scene added

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,7 @@ import Customers from "scenes/customers/Customers";
 import Transactions from "scenes/transactions/Transactions";
 import Geography from "scenes/geography/Geography";
 import Overview from "scenes/overview/Overview";
+import Daily from "scenes/daily/Daily";
 import { themeSettings } from "theme";
 
 function App() {
@@ -30,6 +31,7 @@ function App() {
               <Route path="/transactions" element={<Transactions />} />
               <Route path="/geography" element={<Geography />} />
               <Route path="/overview" element={<Overview />} />
+              <Route path="/daily" element={<Daily />} />
             </Route>
           </Routes>
         </ThemeProvider>
