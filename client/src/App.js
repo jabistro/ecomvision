df911149f8dc52461fb369d878a022f50d421583
John Allan Hinds <jabistro@gmail.com>
2023-03-12 16:46:27 -0400

    monthly scene added

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,7 @@ import Transactions from "scenes/transactions/Transactions";
 import Geography from "scenes/geography/Geography";
 import Overview from "scenes/overview/Overview";
 import Daily from "scenes/daily/Daily";
+import Monthly from "scenes/monthly/Monthly";
 import { themeSettings } from "theme";
 
 function App() {
@@ -32,6 +33,7 @@ function App() {
               <Route path="/geography" element={<Geography />} />
               <Route path="/overview" element={<Overview />} />
               <Route path="/daily" element={<Daily />} />
+              <Route path="/monthly" element={<Monthly />} />
             </Route>
           </Routes>
         </ThemeProvider>
